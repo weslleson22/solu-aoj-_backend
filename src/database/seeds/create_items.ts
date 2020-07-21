038101ac4778a0730459c675018e90beedb9ef1a
weslleson22 <wesllesonsowza@gmail.com>
2020-07-21 19:27:39 -0300

    seeds

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -2,11 +2,11 @@ import Knex from 'knex';
 
 export async function seed(knex: Knex) {
   await knex('items').insert([
-    { title: 'Lâmpadas', image: 'lampadas.svg' },
-    { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
-    { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
-    { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
-    { title: 'Óleo de Cozinha', image: 'oleo.svg' },
+    { title: 'Troca de Pneus', image: 'pneu.svg' },
+    { title: 'Mecânico', image: 'mecanico.svg' },
+    { title: 'Lavagem de Carro', image: 'lavagemdecarro.svg' },
+    { title: 'Checklist', image: 'checklist.svg' },
+    { title: 'Bateria', image: 'bateria.svg' },
+    { title: 'Aliamento de roda', image: 'aliamentoderoda.svg' },
   ]);
 }
