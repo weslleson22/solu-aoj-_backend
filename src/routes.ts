bfb791cdf1e8f752a16a7bb8405411a9e4494b4b
weslleson22 <wesllesonsowza@gmail.com>
2020-08-10 10:39:45 -0300

    salvando no banco

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import knex from './database/connection';
 
 const routes = express.Router();
@@ -8,12 +8,51 @@ routes.get('/items', async(request, response)=>{
 
      const serializedItems = items.map(item =>{
          return{
+             id: item.id,
              title: item.title,
              image_url: `http://localhost:3333/uploads/${item.image}`,
          };
      });
 
      return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response)=>{
+    const{
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    }= request.body;
+        
+        const inserteIds = await knex('points').insert({
+           image:'image-fake',
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf,
+            
+        });
+
+        const point_id = inserteIds[0];
+        const pointItems = items.map((item_id: number)=>{
+            return{
+                item_id,
+                point_id,
+            };
+
+        })
+        await knex('point_items').insert(pointItems);
+        return response.json({success:true});
+
+    
 });
     
 export default routes;
